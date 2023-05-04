f322899bbaa54d2b9663d4ebed6353a2ace2c78d
Hugo Lui <115033116+hugo-lui@users.noreply.github.com>
2023-05-03 23:21:49 -0400

    Add files via upload

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,8 +1,22 @@
 import { NextResponse } from "next/server";
 
+const computer = {"macbook-air": {"price": 1299.00, "stock": 1}, "macbook-pro": {"price": 2599.00, "stock": 1}, "imac": {"price": 1949.00, "stock": 1}};
+const phone = {"iphone-14-pro": {"price": 1399.00, "stock": 1}, "iphone-14": {"price": 1099.00, "stock": 1}, "iphone-13": {"price": 849.00, "stock": 1}, "iphone-se": {"price": 579.00, "stock": 1}};
+const items = {"computer": computer, "phone": phone};
+
 export async function GET(request) {
-  let laptop = {"MacBook Air": {price: 1299.00, stock: 1}, "MacBook Pro": {price: 2599.00, stock: 1}, "iMac": {price: 1949.00, stock: 1}};
-  let phone = {"iPhone 14 Pro": {price: 1399.00, stock: 1}, "iPhone 14": {price: 1099.00, stock: 1}, "iPhone 13": {price: 849.00, stock: 1}, "iPhone SE": {price: 579.00, stock: 1}};
-  const { product } = request.body;
-  return NextResponse.json(phone.product.price);
+  const { searchParams } = new URL(request.url);
+  const product = searchParams.get("product");
+  const level = searchParams.get("level");
+  const param = searchParams.get("param");
+
+  return NextResponse.json(items[product][level][param]);
+}
+
+export async function POST(request) {
+  
+}
+
+export async function PUT(request) {
+  
 }
---
+++ app/cart/page.js
@@ -0,0 +1,10 @@
+export default function Cart(props) {
+    return (
+        <div>
+            <div className="pl-96">
+                <h1 className="font-bold text-4xl pt-16">Review your bag.</h1>
+                <p className="pt-6">Get free shipping and free returns on all orders.</p>
+            </div>
+        </div>
+    );
+}
